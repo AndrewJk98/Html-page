25543105938096657ea04d4c36dbb62997d201d4
Eliseo Cueva <94854195+AndrewJk98@users.noreply.github.com>
2023-04-26 16:14:31 -0500

    Add files via upload

---
+++ prods.js
@@ -0,0 +1,3 @@
+let stockProd=[
+    {OEM: 17801-21050, nombre: "Filtro de Aire", tipo: "fAire", cantidad: 1, desc: "Toyota Corolla 1.6 2007 - 2010", precio: 8.6, talle: "L", img: './Toyota.jpg'}
+];
